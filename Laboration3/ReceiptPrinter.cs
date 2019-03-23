2040526e054845f268258441635f469879127e69
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-03-23 14:30:19 +0100

    Add documentation for the ReceiptPrinter.

--- Laboration3/ReceiptPrinter.cs
+++ Laboration3/ReceiptPrinter.cs
@@ -13,6 +13,9 @@ namespace Laboration3
         private readonly List<ShoppingCartItem> _shoppingCart;
         private double _orderTotal;
 
+        // ReceiptPrinter: Constructor. Sets up the private fields required for the class to function.
+        // Pre: shoppingCart != null.
+        // Post: The _printDialog, _printDocument and _shoppingCart has been setup. 
         public ReceiptPrinter(List<ShoppingCartItem> shoppingCart)
         {
             _printDialog.Document = _printDocument;
@@ -20,6 +23,10 @@ namespace Laboration3
             _shoppingCart = shoppingCart;
         }
 
+        // PrintReceipt: Prints a receipt based on the items in the list _shoppingCart.
+        // Pre: The _shoppingCart is not empty. orderTotal equals the total value of items in the _shoppingCart.
+        // Post: The _orderTotal has been set to the total value of items in the _shoppingCart and a PrintDialog-box has been shown, 
+        // allowing the user to print a receipt for their purchase.
         public void PrintReceipt(double orderTotal)
         {
             _orderTotal = orderTotal;
@@ -27,11 +34,15 @@ namespace Laboration3
                 _printDocument.Print();
         }
 
+        // PrintDocument_PrintPage: Event-handler for when the user uses the _printDialog to print a page.
+        // Pre: -
+        // Post: A receipt has been printed based on the contents of the _shoppingCart.
         private const int _linesPerPage = 40;
         private int _linesToWrite = 0;
         private int _linesWrittenOnPage = 0;
         private int _totalLinesWritten = 0;
-        private bool _isFirstPage = true;
+        private bool _isFirstPage = true; // Used to determine if it's the first page or not. If it's, the other related fields are reset for the new
+                                          // receipt and a header is printed.
         private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
         {
             if (_isFirstPage)
@@ -44,7 +55,7 @@ namespace Laboration3
                 _isFirstPage = false;
             }
 
-            while (_linesWrittenOnPage < _linesToWrite && _linesWrittenOnPage < _linesPerPage)
+            while (_linesWrittenOnPage < _linesToWrite && _linesWrittenOnPage < _linesPerPage) // Writes at most _linesPerPage (currently 40) items per page.
             {
                 string lineForShoppingItem = "Varunummer " + _shoppingCart[_totalLinesWritten].Id.ToString() + ": " +
                                              _shoppingCart[_totalLinesWritten].Name + ", # " +
